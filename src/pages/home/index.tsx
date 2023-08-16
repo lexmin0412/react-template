4e512b1225dbb0921d73370692a41de36922555d
lexmin0412 <zhangle_dev@outlook.com>
2023-08-16 23:58:06 +0800

    feat: 添加 ahooks

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,18 +1,41 @@
-import { Button } from 'antd'
+import { useRequest } from 'ahooks'
+import { Button, message } from 'antd'
 
 function Home() {
+	const {
+		loading: fetchLoading,
+		runAsync: handleFakeFetch
+	} = useRequest(
+    () => {
+      return fetch(window.location.href).then((res) => {
+        return res.text();
+      });
+    },
+    {
+			manual: true,
+      onSuccess: (res) => {
+        console.log("请求结果", res);
+				message.success('请求成功')
+      },
+    }
+  );
 
   return (
     <>
       <div className="m-3">
-        <Button>
-					<a href='/react-template/about'>路由切换</a>
-				</Button>
+        <Button loading={fetchLoading} onClick={handleFakeFetch}>
+          点击请求
+        </Button>
       </div>
       <div className="m-3">
         <Button>
-					<a href='/react-template/antd-demo'>AntD Demo</a>
-				</Button>
+          <a href="/react-template/about">路由切换</a>
+        </Button>
+      </div>
+      <div className="m-3">
+        <Button>
+          <a href="/react-template/antd-demo">AntD Demo</a>
+        </Button>
       </div>
     </>
   );
