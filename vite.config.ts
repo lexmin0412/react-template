b6b988b036390a0578a3f98387f785738eee0981
lexmin0412 <zhangle_dev@outlook.com>
2024-05-26 21:22:53 +0800

    chore: add src alias

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	base: '/react-template/'
+	base: '/react-template/',
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
 })
