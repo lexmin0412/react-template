e184e7194f63b25a31e1fa0c3f92cf1822ef016a
lexmin0412 <zhangle_dev@outlook.com>
2024-11-19 10:59:54 +0800

    fix: 替换遗留的 react-router-dom 引用为 pure-react-router

--- src/pages/antd-demo/index.tsx
+++ src/pages/antd-demo/index.tsx
@@ -1,5 +1,5 @@
 import { Button, DatePicker } from 'antd'
-import { Link } from 'react-router-dom'
+import { Link } from 'pure-react-router'
 import dayjs from "dayjs";
 
 export default function AntdDemo() {
