6e9766bf87b613b14c4171f32f2320d9476705bb
lexmin0412 <zhangle_dev@outlook.com>
2025-01-23 15:32:22 +0800

    feat: 升级 react 到 v19, 升级 antd 到最新版本以适配 v19

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@
 import ReactDOM from "react-dom/client";
 import App from './App'
 import "./index.css";
+import '@ant-design/v5-patch-for-react-19';
 
 const rootDom = document.getElementById("root")
 
