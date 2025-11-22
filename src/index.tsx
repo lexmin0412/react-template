38cf814b2be85c6fbc4dcbf16ad654feaf93b263
lexmin0412 <zhangle_dev@outlook.com>
2025-11-22 22:53:55 +0800

    chore(deps): 升级 ant design 到 v6，移除不再需要的 antd v5 兼容补丁

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@
 import ReactDOM from "react-dom/client";
 import App from './App'
 import "./index.css";
-import '@ant-design/v5-patch-for-react-19';
 
 const rootDom = document.getElementById("root")
 
